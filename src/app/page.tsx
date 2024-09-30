942b94102fe1138281bf18eea339bd2fef297c39
Matthew Guo <matthew@botbuilt.com>
2024-09-30 13:45:53 -0400

    fix image path
    Signed-off-by: Matthew Guo <matthew@botbuilt.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className='h-[calc(100dvh)]'>
         <div className='w-full h-full absolute z-0'>
-          <Image src='/background.webp' alt='yoasobi logo' fill className='object-cover'></Image>
+          <Image src='/background.png' alt='miko!' fill className='object-cover'></Image>
         </div>
     </main>
   )

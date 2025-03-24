e57f85dc2f37e08b1f681048e725c2c76efe6237
Marin Kurokawa <110259342+a-me-lia@users.noreply.github.com>
2025-03-24 16:13:36 -0400

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,17 +11,6 @@ export default function Profile() {
         <header className="text-center text-[#D1636B] font-bold text-4xl">
           Matthew Guo
         </header>
-        <div className="relative w-[150px] h-[150px] rounded-full overflow-hidden border-4 border-[#D1636B]">
-          <div className="absolute inset-0">
-            <Image
-              src="/image.jpg"
-              alt="Profile Picture"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
-          </div>
-        </div>
           <div className="text-[#D1636B] mt-4 text-center">
             <p>
               Connect with me on <a href="https://www.linkedin.com/in/matthew-guo-5ab664293/" className="font-bold">LinkedIn</a>, 
